e41c1e8834014784e45b41c4af447491cdda97ec
Naman <namantanuj@gmail.com>
2020-05-26 16:46:25 +0530

    pleases

--- DL_model.py
+++ DL_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 from keras.models import Sequential
 from keras.layers import MaxPool2D, Conv2D, Flatten, Dense, Dropout
 from keras.datasets import fashion_mnist
