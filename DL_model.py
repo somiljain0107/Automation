818b9adb6404ca1bd6f66a0a7c8eea2f0a82482f
Naman <namantanuj@gmail.com>
2020-05-26 15:39:46 +0530

    5

--- DL_model.py
+++ DL_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 from keras.models import Sequential
 from keras.layers import MaxPool2D, Conv2D, Flatten, Dense, Dropout
 from keras.datasets import fashion_mnist
@@ -8,30 +7,22 @@ from keras.callbacks import Callback
 from keras.utils import np_utils
 from sklearn.metrics import accuracy_score
 import numpy as np
-
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
 X_train = x_train.reshape(60000, 28, 28, 1)
 X_train = X_train.astype('float32')
 X_train = X_train/255.0
-
 X_test = x_test.reshape(10000, 28, 28, 1)
 X_test = X_test.astype('float32')
 X_test = x_test/255.0
-
 y_train_new = np_utils.to_categorical(y_train)
 y_test_new = np_utils.to_categorical(y_test)
-
 model = Sequential()
-
 model.add(Conv2D(32, (3,3), activation = 'relu', input_shape = (28,28,1) ))
 model.add(MaxPool2D(pool_size=(2, 2) ))
 model.add(Flatten())
 model.add(Dense(256, activation = 'relu'))
 model.add(Dense(10,  activation = 'softmax'))          
-
 model.summary()
-
 class myCallback(Callback):
       def on_epoch_end(self, epoch, logs={}):
           
--- ML_model.py
+++ ML_model.py
@@ -8,11 +8,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 datin=pd.read_csv("/root/Automation/train.csv")
-
 data16 = datin[['Pclass', 'Sex', 'Age', 'Parch','SibSp','Fare','Embarked']]
 y=datin['Survived']
-
-
 def MissingValues(pclass, age):
     if pclass==1:
         return 38
@@ -20,7 +17,6 @@ def MissingValues(pclass, age):
         return 30
     else:
         return 25 
-
 i=0
 while (i!=891):
     a= datin['Pclass'][i]
@@ -31,22 +27,17 @@ while (i!=891):
     else:
         i+=1
         continue
-
 X= data16[['Age','Fare']]
-
 pclass = pd.get_dummies(data16['Pclass'], drop_first=True)
 parch = pd.get_dummies(data16['Parch'], drop_first=True)
 sib = pd.get_dummies(data16['SibSp'], drop_first=True) 
 sex = pd.get_dummies(data16['Sex'], drop_first=True) 
 embar = pd.get_dummies(data16['Embarked'], drop_first=True)
 X_new= pd.concat([X, pclass, sib, parch, sex, embar], axis=1)
-
-
 X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size = 0.20, random_state = 42)
 model= LogisticRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-
 report=classification_report(y_test,y_pred)
 accuracy=(cm.diagonal().sum()/cm.sum())*100
 print(report)
